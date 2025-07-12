7931e8310f6571cd0661088d3702f14678fa2546
TSweb <raymond2belinga@gmail.com>
2025-07-12 12:02:25 +0100

    Problemes resolus

--- src/components/Home/CartDropdown.jsx
+++ src/components/Home/CartDropdown.jsx
@@ -1,5 +1,4 @@
 import { memo } from 'react';
-import { Link } from 'react-router-dom';
 import { LucideShoppingBag } from 'lucide-react';
 
 const CartDropdown = memo(({ itemCount, subtotal, onOpenCart }) => {
